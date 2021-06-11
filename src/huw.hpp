6e0a53b8ca0231ee271e0f78246eac43ae85d7c8
tevoran <j.pfeiffer93@gmx.de>
2021-06-11 21:08:31 +0200

    hello world

---
+++ src/huw.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+namespace huw
+{
+
+	
+	
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <huw.hpp>
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+} 
