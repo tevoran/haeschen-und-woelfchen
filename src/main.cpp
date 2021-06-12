407abb82e629470cde37907ea9c7202e51712395
tevoran <j.pfeiffer93@gmx.de>
2021-06-12 15:11:03 +0200

    nicht ausgewählter charakter hält nun an

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main(int argc, char *argv[])
 {
 	huw::game game;
 
-	huw::sprite hase1("../assets/Haeschen1.png", &game, 0, 0, 64, 64, PLAYER_SIZE, PLAYER_SIZE);
-	huw::sprite hase2("../assets/Haeschen1.png", &game, 0, 0, 32, 32, PLAYER_SIZE, PLAYER_SIZE);
-	huw::player player(&hase1, &hase2, &game);
+	huw::sprite hase1("../assets/Haeschen1.png", &game, 0, 0, 32, 32, PLAYER_SIZE, PLAYER_SIZE);
+	huw::sprite wolf("../assets/Woelfchen1.png", &game, 0, 0, 32, 32, PLAYER_SIZE, PLAYER_SIZE);
+	huw::player player(&hase1, &wolf, &game);
 
 	bool quit=false;
 	while(!quit)
